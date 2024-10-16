2c97589b2122a8d66b36724d569467f105f36d36
m1ma0314 <m1ma@ucsd.edu>
2024-10-15 23:54:30 -0400

    Navigation Test States

--- dataset_filtering.py
+++ dataset_filtering.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import altair as alt
 
-file_path = 'dataset.csv'
+file_path = 'filtered_4topics.csv'
 
 df = pd.read_csv(file_path)
 
 df_prevalence = pd.read_csv("prevalence.csv")
 
+grouped_df = df.groupby(['LocationDesc', 'Class'])['Data_Value'].mean().reset_index()
+race_df = df[df['StratificationCategory2']=='Race/Ethnicity']
+sex_df = df[df['StratificationCategory2']=='Gender']
+race_df = race_df.groupby(['LocationDesc', 'Class', 'Stratification2'])['Data_Value'].mean().reset_index()
+sex_df = sex_df.groupby(['LocationDesc', 'Class', 'Stratification2'])['Data_Value'].mean().reset_index()
+
+
+# Create a new column for each state-topic combination with descriptive names
+grouped_df['Column_Name'] = grouped_df['LocationDesc'] + ' ' + grouped_df['Class'] + ' Average%'
+
 ansi = pd.read_csv('https://www2.census.gov/geo/docs/reference/state.txt', sep='|')
 ansi.columns = ['id', 'abbr', 'state', 'statens']
 ansi = ansi[['id', 'abbr', 'state']]
-
-
-
-# select cognitive decline
-cognitive_decline_df = df[df['Class'] == 'Cognitive Decline']
-
-
-# select only 2022
-cognitive_decline_df_22 =cognitive_decline_df[cognitive_decline_df['YearEnd'] == 2022]
 
 us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 
              'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 
@@ -32,25 +33,22 @@ us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
              'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', 
              'District of Columbia']
 
-cognitive_decline_df_22.rename(columns={'LocationDesc': 'state'}, inplace=True)
-cognitive_decline_df.to_csv('cognitive_decline_df.csv', index=False) 
+df['state'] = df['LocationDesc']
+grouped_df['state'] = grouped_df['LocationDesc']
+race_df['state'] = race_df['LocationDesc']
+sex_df['state'] = sex_df['LocationDesc']
 
-state_filtered_df = cognitive_decline_df_22[cognitive_decline_df_22['state'].isin(us_states)]
+
+state_filtered_df = df[df['state'].isin(us_states)]
 
 state_filtered_df = pd.merge(state_filtered_df, ansi, how='left', on='state')
-
-
-#non_state_filtered_df = cognitive_decline_df_22[~cognitive_decline_df['LocationDesc'].isin(us_states)]
-
-#sorted_us_states = sorted(us_states)
-
-#state_id_mapping = {state: idx + 1 for idx, state in enumerate(sorted_us_states)}
-#state_filtered_df['StateID'] = state_filtered_df['LocationDesc'].map(state_id_mapping)
+state_averaged_df = pd.merge(grouped_df, ansi, how='left', on='state')
+race_averaged_df = pd.merge(race_df, ansi, how='left', on='state')
+sex_averaged_df = pd.merge(sex_df, ansi, how='left', on='state')
 
 df_prevalence.rename(columns={'State': 'state'}, inplace=True)
+
 df_prevalence = pd.merge(df_prevalence, ansi, how='left', on='state')
-
-
 
 columns_to_keep = [
     'YearEnd', 'LocationAbbr', 'state', 'id',
@@ -63,10 +61,10 @@ state_filtered_df = state_filtered_df[columns_to_keep]
 state_filtered_df = state_filtered_df[state_filtered_df['Stratification1']=='65 years or older']
 
 
-cognitive_decline_df_22.to_csv('cognitive_decline_df_22.csv', index=False) 
+grouped_df.to_csv('averaged_4topics.csv', index=False)
+state_averaged_df.to_csv('state_averaged_df.csv', index=False)
 state_filtered_df.to_csv('state_filtered_df.csv', index=False)
 df_prevalence.to_csv('prevalence_df.csv', index=False)
 
-max_percentage_df = state_filtered_df.loc[state_filtered_df.groupby('state')['Data_Value'].idxmax()]
-
-max_percentage_df.to_csv('max_percentage_df.csv', index=False)
+race_averaged_df.to_csv('race_averaged_df.csv', index=False)
+sex_averaged_df.to_csv('sex_averaged_df.csv', index=False)
