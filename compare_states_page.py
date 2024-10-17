bf19f337128f292f86b472ee4184535495b10cf1
m1ma <97704603+m1ma0314@users.noreply.github.com>
2024-10-17 09:44:25 -0400

    Update compare_states_page.py

--- compare_states_page.py
+++ compare_states_page.py
@@ -117,7 +117,7 @@ def show_compare_states_page():
 
         color_scale = alt.Scale(
             domain=['Native Am/Alaskan Native', 'Asian/Pacific Islander', 'Black, non-Hispanic', 'White, non-Hispanic', 'Hispanic'],
-            range=['#1f77b4', '#6baed6', '#e377c2', '#ff9896', '#ff7f0e']  
+            range=['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f']  
         )
 
         # Bar chart for Race data comparison for State 1
